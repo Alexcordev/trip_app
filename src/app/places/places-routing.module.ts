b3ad210721c23531f42672ea8ac3a162f38eb651
Alexcor43 <alexcordev@gmail.com>
2019-10-29 14:46:44 -0400

    App Routing Finished and starting places page with tabs

---
+++ src/app/places/places-routing.module.ts
@@ -0,0 +1,68 @@
+import { PlacesPage } from './places.page';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: 'tabs',
+    component: PlacesPage,
+    children: [
+        {path: 'discover', children: [
+            {
+                path: '',
+                loadChildren: './discover/discover.module#DiscoverPageModule'
+            },
+
+            {
+                path: ':placeId',
+                loadChildren: './discover/place-detail/place-detail.module#PlaceDetailPageModule'
+            },
+            {
+                path: '',
+                redirectTo: '/places/tabs/discover',
+                pathMatch: 'full'
+            }
+        ]
+    },
+    {
+        path: 'offer',
+        children: [
+            {
+                path: '',
+                loadChildren: './offers/offers.module#OffersPageModule'
+            },
+            {
+                path: 'new',
+                loadChildren:
+                './offers/new-offer/new-offer.module#NewOfferPageModule'
+            },
+            {
+                path: 'edit/:placeId',
+                loadChildren: './offers/edit-offer/edit-offer.module#EditOfferPageModule'
+            },
+            {
+                path: 'placeId',
+                loadChildren:
+                './offers/offer-booking/offer-booking.module#OfferBookingPageModule'
+            }
+
+        ]
+    }
+    ]
+
+
+   },
+   {
+       path: '',
+       redirectTo: '/places/tabs/discover',
+       pathMatch: 'full'
+   }
+
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forChild(routes)
+  ],
+  exports: [RouterModule]
+})
+export class PlacesRoutingModule {}
